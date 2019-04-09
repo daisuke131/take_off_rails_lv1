af7b06e56546480c5f3d68ff31ad87c329ad2142
daisuke <dada.00131@google.com>
2019-04-09 17:28:14 +0900

    articles追加

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,32 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :update, :destroy]
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def show
+  end
+
+  def create
+    Article.create!(article_params)
+  end
+
+  def update
+    @article.update!(article_params)
+  end
+
+  def destroy
+    @article.destroy!
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
