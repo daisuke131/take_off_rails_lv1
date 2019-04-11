7e698ea129b57a649120dde2f1151edfae1015e4
daisuke <dada.00131@google.com>
2019-04-11 12:53:26 +0900

    Modified index show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,24 +2,34 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.published.all
     render json: @articles
   end
 
   def show
     @article = Article.find(params[:id])
+    case @article.status
+    when "draft"
+      @article = current_user.articles.find(params[:id])
+    when "published"
+      @article = Article.published.find(params[:id])
+    end
+    render json: @article
   end
 
   def create
-    current_user.articles.create!(article_params)
+    @article = current_user.articles.create!(article_params)
+    render json: @article
   end
 
   def update
     @article.update!(article_params)
+    render json: @article
   end
 
   def destroy
     @article.destroy!
+    render json: @article
   end
 
   private
@@ -28,6 +38,6 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
